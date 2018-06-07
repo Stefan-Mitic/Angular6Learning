43c43070218965274c84b4d8de51636de1f7b205
Stefan-Mitic <stefan.mitic@hotmail.com>
2018-06-06 21:22:33 -0400

    created user collection for mongoose

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,16 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const router = express.Router()
+
+const mongoose = require('mongoose')
+const db = "mongodb://adminstefan:A123456@ds151127.mlab.com:51127/angular6"
+
+mongoose.connect(db, err => {
+    if (err) {
+        console.log('Error!' + err)
+    } else {
+        console.log('Connected to mongodb')        
+    }
+})
 
 router.get('/', (req, res) => {
     res.send('From API route')
