a9d64df49dc59b3d7cd8d02439fb09c7ea44eb69
Stefan-Mitic <stefan.mitic@hotmail.com>
2018-06-07 21:16:11 -0400

    updated api to register user

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
 
 const mongoose = require('mongoose')
 const db = "mongodb://adminstefan:A123456@ds151127.mlab.com:51127/angular6"
@@ -14,6 +15,18 @@ mongoose.connect(db, err => {
 
 router.get('/', (req, res) => {
     res.send('From API route')
+})
+
+router.post('/register', (req, res) => {
+    let userData = req.body
+    let user = new User(userData)
+    user.save((error, registeredUser) => {
+        if (error) {
+            console.log(error)
+        } else {
+            res.status(200).send(registeredUser)
+        }
+    })
 })
 
 module.exports = router
