5a909678e6ff3655de65bdc5462719b06f59934c
Stefan-Mitic <stefan.mitic@hotmail.com>
2018-07-05 17:28:39 -0400

    added verify token to api

--- server/routes/api.js
+++ server/routes/api.js
@@ -14,6 +14,22 @@ mongoose.connect(db, err => {
     }
 })
 
+function verifyToken(req, res, next) {
+    if (!req.headers.authorization) {
+        return res.status(401).send('Unauthorized request')
+    }
+    let token = req.headers.authorization.split(' ')[1]
+    if (token === 'null') {
+        return res.status(401).send('Unauthorized request')
+    }
+    let payload = jwt.verify(token, 'secretKey')
+    if (!payload) {
+        return res.status(401).send('Unauthorized request')
+    }
+    req.userId = payload.subject
+    next()
+}
+
 router.get('/', (req, res) => {
     res.send('From API route')
 })
@@ -55,13 +71,13 @@ router.post('/login', (req, res) => {
 
 router.get('/events', (req, res) => {
     // Hard coded list of events
-    // let event = []
+    // let event = [...]
     // res.json(events)
 })
 
-router.get('/special', (req, res) => {
+router.get('/special', verifyToken, (req, res) => {
     // Hard coded list of events
-    // let event = []
+    // let event = [...]
     // res.json(events)
 })
 
